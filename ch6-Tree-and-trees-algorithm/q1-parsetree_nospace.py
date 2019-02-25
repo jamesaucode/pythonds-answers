9cbe64cd9f66be992143bd3f8b1ee960c464fdb6
James <jamesaubudi@gmail.com>
2019-02-25 02:05:53 -0800

    Corrected ch6 q1

--- ch6-Tree-and-trees-algorithm/q1-parsetree_nospace.py
+++ ch6-Tree-and-trees-algorithm/q1-parsetree_nospace.py
@@ -1,40 +1,43 @@
 from pythonds.basic.stack import Stack
 from pythonds.trees.binaryTree import BinaryTree
 
+# ### = changed
+# This question is all about string manipulation
+
 def buildParseTree(fpexp):
-    fplist = fpexp.split()
-    # Split the string argument into array
+    fplist = list(fpexp.replace(" ", "")) ###
     pStack = Stack()
-    # Stack in this case is to store the data of the tree 
-    #(what is the last tree?.. etc)
     eTree = BinaryTree('')
     pStack.push(eTree)
     currentTree = eTree
+    number = "" ###
     for i in fplist:
         if i == '(':
-            # Create a new node as the left child of the root
             currentTree.insertLeft('')
             pStack.push(currentTree)
             currentTree = currentTree.getLeftChild()
         elif i not in ['+', '-', '*', '/', ')']:
-            # So, if it's a number,
-            # Set the value of this node to i
-            currentTree.setRootVal(int(i))
-            # Get the parent tree from the top of the stack
-            parent = pStack.pop()
-            # Go back to the root of the tree (Go up)
-            currentTree = parent
+            number += i ###
         elif i in ['+', '-', '*', '/']:
-            # Set the current node as i (opeartion symbol)
+            ###
+            if number: 
+                currentTree.setRootVal(int(number))
+                parent = pStack.pop()
+                currentTree = parent
+                number = ""
+            ###
             currentTree.setRootVal(i)
-            # Create a new node as the right child of the root
             currentTree.insertRight('')
-            # Saves this current tree on the top of the stack
             pStack.push(currentTree)
-            # Travels to the right child of the current node
             currentTree = currentTree.getRightChild()
         elif i == ')':
-            # Go back one branch if we see a ')'
+            ###
+            if number:
+                currentTree.setRootVal(int(number))
+                parent = pStack.pop()
+                currentTree = parent
+                number = ""
+            ###
             currentTree = pStack.pop()
         else:
             raise ValueError
